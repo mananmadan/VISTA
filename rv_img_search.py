97a6ce0a0de4d0a06a898b75ab271301733f50bb
mananmadan <madan.manan1@gmail.com>
2019-10-20 05:01:59 -0700

    Categories-Layer Working

--- rv_img_search.py
+++ rv_img_search.py
@@ -34,9 +34,38 @@ soup = BeautifulSoup(sourceCode,'html.parser')
  
 city_tags = soup.find_all('input', title="Search")
 print(city_tags[0]['value'])
-eachthing = wikipedia.page(city_tags[0]['value']).categories
-print(eachthing)
 
+#eachthing = wikipedia.page(city_tags[0]['value']).categories
+#print(eachthing)
+wikipedia.set_lang("en")
+#query = (city_tags[0]['value'])
+query = "Python"
+WikiPage = wikipedia.page(title = query,auto_suggest = True)
+var1="Articles"
+var2="articles"
+var3="Wikipedia"
+var4="wikipedia"
+cat = WikiPage.categories
+for i in cat:
+    result=i.find(var1)
+    result+=i.find(var2)
+    result+=i.find(var3)
+    result+=i.find(var4)
+    if result  == -4: 
+        print (i)
+        print("*******************************************************\n")
+        query=i
+        WikiPage = wikipedia.page(title = query,auto_suggest = True)
+        cat1 = WikiPage.categories
+        for j in cat1:
+         result1=j.find(var1)
+         result1+=j.find(var2)
+         result1+=j.find(var3)
+         result1+=j.find(var4)
+         if result1  == -4: 
+          print (j)
+        print("--------------------------------------------------------\n")
+      
 
 
 
