cda7df5857f234bac16e6281360ef514329197cb
mananmadan <madan.manan1@gmail.com>
2019-10-19 23:37:42 -0700

    value extracted

--- rv_img_search.py
+++ rv_img_search.py
@@ -5,6 +5,7 @@ import urllib2
 from urllib2 import urlopen
 from cookielib import CookieJar
 import time
+from bs4 import BeautifulSoup
 cj = CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
@@ -12,27 +13,31 @@ opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
 opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17')]
 
-def imageLookup():
+#def imageLookup():
     #s = sftp.Connection(host = "http://pythonprogramming.net",
-    #                   username = "python",
-    #                    password = "rules")
-    #localpath = '/pic.png'
+                       #username = "python",
+                        #password = "rules")
+    #localpath = ''
     #numToAdd = str(int(time.time()))
-    #remotepath = '/var/www/imagerec/currentImage'+numToAdd+'.png'
+    #remotepath = '/var/www/imagerec/currentImage'+numToAdd+'.jpg'
     #s.put(localpath,remotepath)
     #s.close()
-    #pythonprogramming.net/imagerec/currentImage
+    #imagepath = 'http://pythonprogramming.net/imagerec/currentImage'+numToAdd+'.jpg'
+imagepath = 'https://github.com/shikhar-scs/Keldec_dataset/blob/master/dataset_2/image2.jpg?raw=true'
+googlepath = 'http://images.google.com/searchbyimage?image_url='+imagepath
 
-    imagepath = 'http://i.gyazo.com/82ed00642007a857db454cfd19034cae.png'
-    googlepath = 'http://images.google.com/searchbyimage?image_url='+imagepath
+sourceCode = opener.open(googlepath).read()
+soup = BeautifulSoup(sourceCode,'html.parser')       
+ 
+city_tags = soup.find_all('input', title="Search")
+print(city_tags[0]['value'])
+   
 
-    sourceCode = opener.open(googlepath).read()
 
-    
-
+#imageLookup()
 
 
-imageLookup()
+
 
 
 
