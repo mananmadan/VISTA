667dfbea137a0612b56ebd131a6e6a1b4f4e5b4d
mananmadan <madan.manan1@gmail.com>
2019-10-29 00:13:41 -0700

    Graph page exception not working

--- temporary.py
+++ temporary.py
@@ -10,6 +10,68 @@ import wikipedia
 import scrapy
 from scrapy import *
 
+class Graph:
+
+
+    
+
+
+    graph_dict={}
+
+
+    
+
+
+    def addEdge(self,node,neighbour):  
+
+
+        if node not in self.graph_dict:
+
+
+            self.graph_dict[node]=[neighbour]
+
+
+        else:
+
+
+            self.graph_dict[node].append(neighbour)
+
+
+            
+
+
+    def show_edges(self):
+
+
+        for node in self.graph_dict:
+
+
+            for neighbour in self.graph_dict[node]:
+
+
+                print("(",node,", ",neighbour,")")
+
+    
+def find_path(self, start_vertex, end_vertex, path=None):
+        if path == None:
+            path = []
+        graph = self.__graph_dict
+        path = path + [start_vertex]
+        if start_vertex == end_vertex:
+            return path
+        if start_vertex not in graph:
+            return None
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                extended_path = self.find_path(vertex, 
+                                               end_vertex, 
+                                               path)
+                if extended_path: 
+                    return extended_path
+        return None
+
+
+g= Graph()
 cj = CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
@@ -20,6 +82,7 @@ opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/5
 #eachthing = wikipedia.page(city_tags[0]['value']).categories
 #print(eachthing)
 wikipedia.set_lang("en")
+
 
 def my_f(query):
  content = opener.open(wikipedia.page(query).url).read()
@@ -32,12 +95,26 @@ def my_f(query):
   if d != 0:
    list.append(str(i.text))
   d=d+1
- print(query,":",list,"\n")
+ #print(query,":",list,"\n")
  return list
 
 query = "Rock Concert"
 
-templist = my_f(query)
+def create_graph(query,lvl,g):
+ lvl = lvl + 1
+ templist = my_f(query)
+ print(templist)
+ print("\n")
+ for i in templist:
+  g.addEdge(query, i)
+ for j in templist:
+  if lvl<15 :
+   create_graph(j,lvl,g)
+  else :
+   return 0
+   
+
+create_graph(query,0,g)
  
 
  
