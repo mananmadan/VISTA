73303f9e4f945be66b929761064ac1b392149ba6
gupta2022 <guptagaurav2022@gmail.com>
2019-11-02 09:09:58 +0000

    Testing

--- temp2.py
+++ temp2.py
@@ -25,7 +25,7 @@ def generate_edges(graph):
 def show_edges(graph):
     edges=generate_edges(graph)
     for n in edges:
-     print(n)
+     print ("n")
 def find_shortest_path(graph, start, end, path =[]):
         path = path + [start]
         if start == end:
@@ -53,39 +53,50 @@ opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/5
 #print(eachthing)
 wikipedia.set_lang("en")
 def my_f(query):
+ #wikiterm = wikipedia.search(query)
+ dx = 0
+ try:
+    page = wikipedia.page(query)
+ except wikipedia.exceptions.DisambiguationError as e:
+  dx=10
+ except wikipedia.exceptions.PageError as e:
+  #print e
+  dx=10
+ if dx == 0:
+  if urllib.urlopen(wikipedia.page(query).url).getcode() == 200 :
+   content = opener.open(wikipedia.page(query).url).read()
+   soup2 = BeautifulSoup(content,'html.parser')
+   cat1 = soup2.find_all("div",{'class':'mw-normal-catlinks'})
+   cat2 = cat1[0].find_all('a')
+   d=0
+   list=[]
+   for i in cat2:
+    if d != 0:
+     list.append(str(i.text))
+    d=d+1
+ else:
+     return 0
+ return list
 
- if urllib.urlopen(wikipedia.page(query).url).getcode() == 200 :
-  content = opener.open(wikipedia.page(query).url).read()
-  soup2 = BeautifulSoup(content,'html.parser')
-  cat1 = soup2.find_all("div",{'class':'mw-normal-catlinks'})
-  cat2 = cat1[0].find_all('a')
-  d=0
-  list=[]
-
-  for i in cat2:
-   if d != 0:
-    list.append(str(i.text))
-   d=d+1
-
-  return list
-
-query = "Food"
+query = "traffic signals"
 
 def create_graph(query,lvl):
  lvl = lvl + 1
-
  templist = my_f(query)
- print(query," ",templist)
- print("\n")
- for i in templist:
-  addEdge(graph,query, i)
- for j in templist:
-  #print(j)
-  if lvl<4 :
-   if j !=query :
-    create_graph(j,lvl)
-  else :
-   return 0
+ if templist != 0 and len(templist) != 0 :
+  print(query," ",templist)
+  print("\n")
+  for i in templist:
+   addEdge(graph,query, i)
+  for j in templist:
+   #print(j)
+   if lvl<4 :
+    if j !=query :
+     create_graph(j,lvl)
+   else:
+    return 0
+ else:
+    return 0
 create_graph(query,0)
 show_edges(graph)
 path2=[]
