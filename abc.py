d9077104bf6bdc8b40e1dac01edf0a34b263ed3d
Manan Madan <madan.manan1@gmail.com>
2020-01-10 05:46:05 +0000

    bug fixed

--- abc.py
+++ abc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import pysftp as sftp
+#import pysftp as sftp
 import urllib2
 from urllib2 import urlopen
 from cookielib import CookieJar
@@ -117,7 +117,7 @@ def create_graph(query,lvl,graph):
     addEdge(graph,query, i)
   for j in templist:
    #print(j)
-   if lvl<3:
+   if lvl<2:
     if j !=query :
      create_graph(j,lvl,graph)
    else:
@@ -178,7 +178,44 @@ def read_from_file(graph,name):
 
 graph = defaultdict(list)
 
-xl=["expert system","decision making","specific domain","less","artificial intelligence","powerful computers","cpu","ai","ai","computers","digital equipment 's corporations","dec","ax","typical system","ai s w","ai","high cost","wide range","h w","s w","conventional computers","computer","computer programs","conventional languages","c++","cobol","conventional computers","conventional software","intelligent computer","artificial","intelligence software","artificial","intelligence languages","lisp","list","prlog","logic","intelligent computers","language translate","robotic","system air","travel","processing expert","conventional software","artificial","conventional computer","step procedure","mathematical formula","sequential procedure","computing","quantitative problems","ai","qualitative problem","conventional software system","doing","laborious jobs","conventional computer system","loop statements","major application area","ai","language","robotics expert","computer vision"]
+xl = ["modern encryption techniques","digital","binary digits","modern cryptosystems need","binary strings",
+"binary string","based","binary strings","symmetric encryption schemes","block ciphers","plain binary text",
+"plaintext bits","ciphertext bits","des","aes","block sizes","stream ciphers","time i.e","technically","stream ciphers",
+"block ciphers","block size","block cipher","basic scheme","block cipher","block cipher","plaintext bits","ciphertext bits",
+"block size","encryption scheme","key length","block size","avoid","small block size","say","block size","m bits",
+"possible plaintext bits combinations","attacker discovers","plain text blocks","ciphertext blocks","dictionary attack",
+"plaintext_ciphertext pairs","encryption key","block size","dictionary needs","large block size","large block size",
+"such plaintexts","multiples","block size","computer processor","padding","block cipher block","ciphers process blocks",
+"block size","bit plaintext","bits needs","redundant information","final block","bits need","redundant bits","complete block",
+"too","system insecure","block cipher schemes","vast number","block ciphers schemes","prominent block ciphers","digital encryption",
+"des","popular block cipher","_broken_ block cipher","small key size","triple des","variant scheme","des","block ciphers","block ciphers",
+"advanced encryption","aes","new block cipher","encryption algorithm","rijndael","aes","design competition","idea","strong block cipher",
+"block size","key size","idea","early versions","privacy","pgp","idea","patent issues","feistel block cipher","feistel cipher",
+"specific scheme","block cipher","design model","different block ciphers","des","feistel cipher","cryptographic system","feistel",
+"cipher structure uses","encryption process","encryption process uses","feistel","multiple rounds","permutation step","feistel structure",
+"input block","encryption key","function _f_","r.","output f","xor","mathematical function","l.","real implementation","feistel cipher",
+"des","whole encryption key","dependent key","encryption key","round uses","different key","original key","permutation step","round swaps",
+"r.","current round","output l","current round","permutation steps form","algorithm design","sub blocks","ciphertext block","difficult part",
+"feistel cipher","round function _f_","unbreakable scheme","function needs","important properties","decryption process","feistel",
+"ciphertext block","feistel","reverse order","feistel cipher","number","rounds","feistel cipher","inefficient slow encryption",
+"decryption processes","number","efficiency security tradeoff","data encryption","des","symmetric key block cipher",
+"national institute","standards","nist","des","feistel cipher","feistel","block size","key length","des","effective key length",
+"encryption algorithm","check bits","structure","des","des","feistel cipher","des","round function","key","additional processing",
+"initial","final permutation","initial","final permutation","final permutations","permutation","cryptography significance","des",
+"final permutations","function","des","des","function applies","bit key","bit output","expansion permutation","right input","round key",
+"permutation","permutation logic","des","key generation","round key generator creates sixteen","bit keys","bit cipher key","key generation",
+"parity","compression p","des","des analysis","des","block cipher","avalanche","plaintext results","completeness","des","weak keys","des",
+"block cipher","significant cryptanalytic attacks","des","exhaustive key search","key triple des","3tdes key","des","actual 3tdes key",
+"encryption scheme","encryption decryption process","encrypt","plaintext blocks","des","des","des","decryption","reverse process","user",
+"triple des","encrypt decrypt encrypt process","des","k2","backwards compatibility","des","triple des","user encrypt plaintext blocks",
+"key length","triple des","des","des","advanced encryption","symmetric encryption algorithm","advanced encryption","aes","des","des",
+"key size","exhaustive key search attack","triple des","aes","symmetric","key symmetric block cipher","bit data","128/192/256 bit keys",
+"stronger","triple des provide","full specification","design details","software","java","aes aes","feistel",
+"_substitution permutation network_","specific outputs","interestingly","aes","hence","aes","plaintext block",
+"des","aes","aes","bit keys","bit keys","bit keys","rounds uses","bit round key","aes","aes","encryption process",
+"typical round","aes","sub processes","round process"
+]
+
 for query in xl:
    read_from_file(graph,query)
    create_graph(query,0,graph)    
