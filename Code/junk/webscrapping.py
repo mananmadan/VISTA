4345b0fee6f12214f0b79cca313182b2d113c86d
mananmadan <madan.manan1@gmail.com>
2020-07-17 16:22:22 +0530

    added some sample graphs

--- Code/junk/webscrapping.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import requests
-from bs4 import BeautifulSoup
-url = 'http://web.mta.info/developers/turnstile.html' 
-response = requests.get(url)
-soup = BeautifulSoup(response.text,'html.parser')
-soup.findAll('a')
-
-
